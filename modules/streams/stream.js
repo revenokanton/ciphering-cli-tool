d7418fcd98e826aa4f7709ae08ac0f56321ae047
antonrevenok <anton.revenok@informa.com>
2021-11-09 11:26:45 +0300

    Add comments

--- modules/streams/stream.js
+++ modules/streams/stream.js
@@ -2,11 +2,21 @@ const { getInputFile, getOutputFile } = require("../helpers/args");
 const fs = require("fs");
 const path = require("path");
 
+/**
+ * Return read stream
+ *
+ * @returns {*} - Read stream or stdin
+ */
 const getReadStream = () => {
   const input = getInputFile();
   return input ? fs.createReadStream(path.join(input)) : process.stdin;
 };
 
+/**
+ * Return write stream
+ *
+ * @returns {*} - Write stream or stdout
+ */
 const getWriteStream = () => {
   const output = getOutputFile();
   return output
