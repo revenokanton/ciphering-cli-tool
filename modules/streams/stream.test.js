14c113a3a7e6a2471e42ec3dce22e6c6c792f977
antonrevenok <anton.revenok@informa.com>
2021-11-19 22:47:23 +0300

    Add unit tests for stream.js

---
+++ modules/streams/stream.test.js
@@ -0,0 +1,35 @@
+const { expect } = require("@jest/globals");
+const { getReadStream, getWriteStream } = require("./stream");
+const fs = require("fs");
+const path = require("path");
+
+describe("modules / streams / stream / getReadStream", () => {
+  it("should return read stream", () => {
+    expect(() => {
+      getReadStream(["-c", "C1-C0", "-i", "input.txt"]);
+    }).not.toThrow();
+  });
+  it("should return stdin if no error", () => {
+    jest.clearAllMocks();
+    jest.spyOn(console, "log");
+    expect(() => {
+      getReadStream(["-c", "C1-C0"]);
+    }).not.toThrow();
+    expect(console.log.mock.calls[0][0]).toEqual("Enter text:");
+  });
+});
+
+describe("modules / streams / stream / getReadStream", () => {
+  it("should return read stream", () => {
+    expect(() => {
+      getWriteStream(["-c", "C1-C0", "-o", "output.txt"]);
+    }).not.toThrow();
+  });
+  it("should return stdin if no error", () => {
+    jest.clearAllMocks();
+    jest.spyOn(console, "log");
+    expect(() => {
+      getWriteStream(["-c", "C1-C0"]);
+    }).not.toThrow();
+  });
+});
